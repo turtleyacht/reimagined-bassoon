dc4c76979c7b4aad311da3245bb28bb5d4d6f717
Christopher Cha <16641864+turtleyacht@users.noreply.github.com>
2024-11-14 03:56:33 -0600

    Prompt for input in a loop and sketch out word add
    We need to display numbers next to the board so user can specify where
    to start populating a word. And asking for a word.

--- hello.c
+++ hello.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "board.c"
 
 extern char letters[LEN];
@@ -7,25 +8,49 @@ extern short int states[LEN];
 
 #define TERMINAL_LEN 1000
 
+void prompt(void);
+
 int main(void) {
   printf("%lu\n", sizeof(letters));
 
   char choice;
   char display[TERMINAL_LEN];
+  char word_to_add[LEN];
   int i;
 
-  printf("(d)isplay, (q)uit\n");
-  printf("? ");
+  prompt();
 
-  switch(choice = getchar()) {
-    case 'd':
-      render_board(display, TERMINAL_LEN);
-      printf("%s\n", display);
-      break;
-    case 'q':
-      break;
+  while ((choice = getchar())) {
+    switch(choice) {
+      case 'a':
+        render_board(display, TERMINAL_LEN);
+        printf("%s\n\n", display);
+        printf("word? ");
+        scanf("%s", word_to_add);
+        /* toupper */
+        printf("Begin %s at which position? ", word_to_add);
+        /* scanf or getchar() */
+        /* search valid directions only */
+        printf("direction? ");
+        /* getchar() */
+        break;
+      case 'd':
+        render_board(display, TERMINAL_LEN);
+        printf("%s\n", display);
+        break;
+      case 'q':
+        exit(0);
+        break;
+      default:
+        prompt();
+        break;
+    }
   }
 
   return 0;
 }
 
+void prompt() {
+  printf("(a)dd word, (d)isplay, (q)uit\n");
+  printf("? ");
+}
