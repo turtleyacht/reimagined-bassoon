b13e4f57eaf5db440517c7c4c613989301a44dfc
Christopher Cha <16641864+turtleyacht@users.noreply.github.com>
2024-11-16 21:36:35 -0600

    Display last column number to right of each row
    On the board, we show the last index for each row. The user can then put
    a number between that and some smaller number to designate a position on
    a particular row. It would be difficult otherwise to choose where to put
    the first letter of a word to add.
    
    Where to put the info was easy enough: just before printing a newline.
    How and what to print took a bit longer.
    
    We have to fill in an array representing a display one character at a
    time; for boards with more than 9 positions, we need to calculate each
    digit. So position 18 would mean adding a `1' and an `8' next to each
    other to display that.
    
    Textbooks have solutions for this, but wanted to see if I could write my
    own. Exponentiation could also come from math.h, but I wanted to try a
    "multiply as a series of adds" to explore the link between summation and
    multiplication.
    
    The row_number() function shows some of my initial attempt. It took a
    couple days of thinking off-and-on to break it into two concepts: base
    ten processing for one, and peeling off digits one at a time as the
    other.
    
    Next commit, I'll remove the diagnostic and exploratory function. Just
    wanted a record of what I had tried in the history.

--- board.c
+++ board.c
@@ -15,7 +15,11 @@
    Or just me?
 */
 
+/* Total compact length of our string representation (_not_ the display "string") */
 #define LEN 24
+
+/* Number of digits in LEN */
+#define NUMERIC_PLACES 2
 
 /* These variables describe the board. */
 
@@ -48,7 +52,11 @@ short int states[LEN]    = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 1,
 */
 
 int clear_display(char[], int);
+int fill_positions(int, int, char[]);
+char ntoc(int);
+int powten(int);
 int render_board(char[], int);
+char row_number();
 
 int clear_display(char data[], int len) {
   int i;
@@ -73,31 +81,42 @@ int render_board(char fill[], int len) {
   int st = 0;
   char last_adjacent = '?';
 
+  /* Tells us next index okay to fill */
+  int offset = -1;
+
   /* Maybe `return' was treated as a jump like `goto', so both were normal. */
   /* Maybe you can tell by implementation if a function is a wrapper or low-level. */
   /* Wrapper: reads more like a paragraph than low-level twiddling */
+  /* Granularity in interactivity reflects how many knobs can be adjusted in auto. */
 
   for (bi = 0; bi < LEN; bi++) {
     /* fill[di++] = ' '; */
     fill[di++] = letters[bi];
     /* fill[di++] = ' '; */
 
-    printf("%d %c\n", st, adjacents[bi]);
-
     switch(adjacents[bi]) {
       case '3':
         if (last_adjacent == '5' && st == 0) {
+          offset = fill_positions(bi, di, fill);
+          di += offset;
+          fill[di++] = ';';
           fill[di] = '\n'; /* first row, second corner */
           st++;
         } else if (last_adjacent == '5' && st == 1) {
           st++; /* last row, first corner */
         } else if (last_adjacent == '5' && st == 2) {
+          offset = fill_positions(bi, di, fill);
+          di += offset;
+          fill[di++] = ';';
           fill[di] = '\n'; /* last row, second corner */
           st = 0;
         }
         break;
       case '5':
         if (last_adjacent == '8') {
+          offset = fill_positions(bi, di, fill);
+          di += offset;
+          fill[di++] = ';';
           fill[di] = '\n';
         }
         break;
@@ -112,4 +131,114 @@ int render_board(char fill[], int len) {
   fill[di] = '\0';
 
   return 0;
+}
+
+/* Fill the board with the terminal position of current row.
+   Return offset to caller.
+*/
+int fill_positions(int i, int start, char fill[]) {
+  int numeral = -1;
+  char digit = '?';
+  int exp;
+  int pos = start;
+  int tens_counter = NUMERIC_PLACES - 1; /* ugh, minus 1 on a constant */
+
+  /*
+     173 is 100 + 70 + 3
+     173 has 3 NUMERIC_PLACES
+     NUMERIC_PLACES is 0-based: 2, 1, 0
+     173 / 100 = 1   173 / (10^2) = 1
+     173 % 100 = 73  173 % (10^2) = 73
+     73 / 10 = 7     73 / (10^1) = 7
+     73 % 10 = 3     73 % (10^1) = 3
+     3 / 1 = 3       3 / (10^0) = 3
+     3 % 1 = 3       3 % (10^0) = 3
+   */
+
+  while (tens_counter >= 0) {
+    exp = powten(tens_counter);
+    if (exp > i) {
+      numeral = i;
+    } else {
+      numeral = i / exp;
+    }
+
+    printf("i: %d, start: %d, numeral: %d\n", i, start, numeral);
+    digit = ntoc(numeral);
+
+    if (exp > i && tens_counter > 0) {
+      fill[pos++] = '_';
+    } else {
+      fill[pos++] = digit;
+    }
+    i = i % exp;
+    tens_counter--;
+  }
+  return pos - start;
+}
+
+/* Convert numeral integer to its character representation. */
+char ntoc(int numeral) {
+  assert(numeral < 10);
+  switch (numeral) {
+    case 0: return '0';
+    case 1: return '1';
+    case 2: return '2';
+    case 3: return '3';
+    case 4: return '4';
+    case 5: return '5';
+    case 6: return '6';
+    case 7: return '7';
+    case 8: return '8';
+    case 9: return '9';
+    default: assert(1 == 2);
+  }
+}
+
+/* 10^0 = 1 (base case)
+   10^1 = 10 * 1 = 10 = 10 plus itself 0 times
+   10^2 = 10 * 10 = 100 = 10 plus itself 9 times
+   10^3 = 10 * 10 * 10 = 1000 = ((10 plus itself 9 times) plus itself 9 times)
+*/
+int powten(int n) {
+  assert(n <= NUMERIC_PLACES);
+
+  if (n == 0) {
+    return 1;
+  } else if (n == 1) {
+    return 10;
+  }
+
+  int base = 10;
+  int i, j;
+  int sum = 10;
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < 9; j++) {
+      sum += base;
+    }
+    base = sum;
+  }
+  return sum;
+}
+
+/* Returns pos-th number of the row. For a board of LEN, if it's >=10,
+   row 10 would yield 1 for first position and 0 for second position.
+*/
+char row_number(int i, int pos) {
+  static int places = -1;
+
+  if (places < 0) {
+    int length = LEN;
+    int j = 0;
+    while (length > 0) {
+      length /= 10;
+      j++;
+    }
+
+    places = j;
+    assert(j > 0);
+  }
+
+  assert(pos <= places);
+  return 'o';
 }
