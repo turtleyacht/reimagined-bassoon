ac24a6ef512369e03e2bc4979d1842beaa1c1e45
Christopher Cha <16641864+turtleyacht@users.noreply.github.com>
2024-12-10 05:14:14 -0600

    Separate usage of data index as board index
    Checkpoint commit. We were iterating on directions[], but we were also
    calculating `max_extents_index' per row. For positions like nw (0),
    n (1), and ne (2), which are on the "top row" of our clockwise compass,
    the board row is "zero" but the index `i' is 0, 1, and 2.
    
    We actually just need to calculate the "current row" based on `start',
    and use that throughout to check the eight directions. It's a subtle bug
    but we did find it.
    
    This is the perfect example of small changes that balloon the diff, that
    would motivate small commits. But it's hard to pin down C methods as we
    are noodling away. With unit testing, we'd have to tweak the tests too.
    (But that would help solidify things.)

--- board.c
+++ board.c
@@ -51,14 +51,39 @@ short int states[LEN]    = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 1,
    The board will be too large to do by hand.
 */
 
+int board_row(int);
 int clear_display(char[], int);
 int fill_positions(int, int, char[]);
-int fill_directions(int[]);
+int fill_directions(int[], int);
 int fit_word(char[], int);
 char ntoc(int);
 int powten(int);
 int render_board(char[], int);
+int reset_direction_bound(int[], int, int, int, int, int, int);
 int row_offset(void);
+
+/* Determine which row a board index falls on. Board is stored as a string. */
+int board_row(int position) {
+  assert(position >= 0 && position < LEN);
+
+  int current_row = 0;
+
+  int max_extents_index = LEN;
+
+  /* Our "unit ruler." */
+  int window_size = row_offset();
+
+  /* Offset becomes an absolute. */
+  int row_len = window_size + 1;
+
+  for (; position <= max_extents_index; current_row++) {
+    max_extents_index = (current_row * row_len) + window_size;
+  }
+
+  assert(max_extents_index < LEN);
+
+  return current_row;
+}
 
 int clear_display(char data[], int len) {
   int i;
@@ -69,91 +94,10 @@ int clear_display(char data[], int len) {
   return 0;
 }
 
-/* A "unit ruler" based on the first row */
-/* Treated as an offset, it becomes a window like so:
-
-               0             1             2             3             4
-   A A A A A A _ x x x x x _ _ x x x x _ _ _ x x x _ _ _ _ x x _ _ _ _ _ x
-   A A A A A A             x           x x         x x x       x x x x
-   A A A A A A
-   A A A A A A           5
-               _ _ _ _ _ _
-               x x x x x
-
-   Underscores are before the given index, inclusive of index.
-   The x's always cover or highlight one before the index, in the next row.
-
-   (The last row is a special case.)
-
-   Now we can build up some semantics. Based on surroundings,
-
-     0 1 2 Direction
-     7   3   0 (nw): ?
-     6 5 4   1 (up): ?
-             2 (ne): ?
-             3 (right): index+1
-             4 (se): offset+2
-             5 (down): offset+1
-             6 (sw): offset
-             7 (left): index-1
-
-   What does the window look like as a negative offset?
-
-     x x x x x     x x x x       x x x         x x           x
-   0             1             2             3             4             5
-   _           x _         x x _       x x x _     x x x x _   x x x x x _
-
-   We can fill in the rest, with a caveat (*) for last position in a row:
-
-     0 (nw): -offset-2
-     1 (up): -offset-1
-     2 (ne): -offset (*)
-
-   At the last position in the row, ne is invalid (and technically se too).
-
-   The last index in a row should always be equal to
-
-     row-size = offset + 1
-     try-last-row-index = ([0, 1, ..., LEN-1] * row-size) + offset
-
-   Where we would loop over 0..LEN-1.
-
-   Concretely, our sample board above has last-row indices of 5, 11, 17, and 23.
- */
-int row_offset() {
-  assert(LEN > 1); /* Avoid pathological size-1 "boards." */
-  assert(adjacents[0] == '3'); /* All boards start with a "nw corner." */
-  int i;
-  for (i = 1; i < LEN; i++) {
-    if (adjacents[i] == '3') {
-      break;
-    }
-  }
-  assert(i < LEN); /* All boards end with a "se corner." */
-  return i;
-}
-
-int fill_directions(int directions[]) {
-  /* assert directions is length 8 */
-  return -1;
-}
-
-/* Return direction as numeric just like in states[]
-   The directions are clockwise, so
-
-     0 1 2
-     7   3          0 1 2 3 4 5 6 7
-     6 5 4
-*/
-int fit_word(char word[], int start) {
-  printf("Fit %s at position %d\n", word, start);
-  /* Build out a direction map, i.e. orient yourself */
-  /* Search is travel. */
-
-  /* Record what neighbors return */
-  int directions[] = { -1, -1, -1, -1, -1, -1, -1, -1 };
-
-  int i, max_rows = 0;
+/* Return derived result of the maximum rows */
+int fill_directions(int directions[], int start) {
+  int current_row = 0;
+  int i, max_i = -1, max_i_calculated = 0;
 
   int window_size = row_offset(); /* unit ruler idea */
 
@@ -171,17 +115,22 @@ int fit_word(char word[], int start) {
     directions[0], directions[1], directions[2], directions[3],
     directions[4], directions[5], directions[6], directions[7]);
 
-  /* Sanity check the bounds */
+  /* To make refactoring easier: init vs assignment close to usage site */
+  current_row = board_row(start);
+
   for (i = 0; i < 8; i++) {
     /* The largest index for the current row */
-    int max_extents_index = (i * (window_size + 1)) + window_size;
+    /* TODO determine absolute_row because i is a data index, not board index */
+    int max_extents_index = (current_row * (window_size + 1)) + window_size;
 
     if (max_extents_index < LEN) {
-      max_rows++;
+      max_i = i;
+    } else {
+      max_i_calculated = 1;
     }
 
     max_extents_index = max_extents_index > LEN
-      ? ((max_rows - 1) * (window_size + 1)) + window_size
+      ? (max_i * (window_size + 1)) + window_size
       : max_extents_index;
 
     /* The smallest index for the current row */
@@ -191,19 +140,83 @@ int fit_word(char word[], int start) {
       i, window_size, window_size);
 
     printf("min_extents_index: %d\n", min_extents_index);
-    printf("max_rows: %d\n", max_rows);
+    printf("max_i: %d\n", max_i);
 
-    /* Either keep or reset the index for that direction */
-    /* TODO switch case directions & logic */
-    if (directions[i] < min_extents_index || directions[i] > max_extents_index) {
-      printf("Not keeping %d; reset.\n", directions[i]);
-      directions[i] = -1;
-    }
+    /* Sanity check the bounds */
+    /* TODO prefix with maybe_ */
+    reset_direction_bound(directions, start, i, min_extents_index, max_extents_index,
+      max_i, max_i_calculated);
 
     assert(max_extents_index < LEN);
   }
 
-  assert(max_rows > 0);
+  assert(max_i > 0);
+
+
+  return max_i;
+}
+
+/* Fill the board with the terminal position of current row.
+   Return offset to caller.
+*/
+int fill_positions(int i, int start, char fill[]) {
+  int numeral = -1;
+  char digit = '?';
+  int exp;
+  int pos = start;
+  int tens_counter = NUMERIC_PLACES - 1; /* ugh, minus 1 on a constant */
+
+  /*
+     173 is 100 + 70 + 3
+     173 has 3 NUMERIC_PLACES
+     NUMERIC_PLACES is 0-based: 2, 1, 0
+     173 / 100 = 1   173 / (10^2) = 1
+     173 % 100 = 73  173 % (10^2) = 73
+     73 / 10 = 7     73 / (10^1) = 7
+     73 % 10 = 3     73 % (10^1) = 3
+     3 / 1 = 3       3 / (10^0) = 3
+     3 % 1 = 0       3 % (10^0) = 0
+   */
+
+  while (tens_counter >= 0) {
+    exp = powten(tens_counter);
+    if (exp > i) {
+      numeral = i;
+    } else {
+      numeral = i / exp;
+    }
+
+    digit = ntoc(numeral);
+
+    if (exp > i && tens_counter > 0) {
+      fill[pos++] = '_';
+    } else {
+      fill[pos++] = digit;
+    }
+    i = i % exp;
+    tens_counter--;
+  }
+  return pos - start;
+}
+
+/* Return direction as numeric just like in states[]
+   The directions are clockwise, so
+
+     0 1 2
+     7   3          0 1 2 3 4 5 6 7
+     6 5 4
+*/
+int fit_word(char word[], int start) {
+  printf("Fit %s at position %d\n", word, start);
+  /* Build out a direction map, i.e. orient yourself */
+  /* Search is travel. */
+
+  /* Record indices for each direction */
+  int directions[] = { -1, -1, -1, -1, -1, -1, -1, -1 };
+
+  int max_rows = fill_directions(directions, start);
+
+
 
   /* Check if letter already occupies that direction (states[]) */
 
@@ -256,6 +269,50 @@ int fit_word(char word[], int start) {
 
   int neighbors[8] = { -1 };
   return 0;
+}
+
+/* Convert numeral integer to its character representation. */
+char ntoc(int numeral) {
+  assert(numeral < 10);
+  switch (numeral) {
+    case 0: return '0';
+    case 1: return '1';
+    case 2: return '2';
+    case 3: return '3';
+    case 4: return '4';
+    case 5: return '5';
+    case 6: return '6';
+    case 7: return '7';
+    case 8: return '8';
+    case 9: return '9';
+    default: assert(1 == 2);
+  }
+}
+
+/* 10^0 = 1 (base case)
+   10^1 = 10 * 1 = 10 = 10 plus itself 0 times
+   10^2 = 10 * 10 = 100 = 10 plus itself 9 times
+   10^3 = 10 * 10 * 10 = 1000 = ((10 plus itself 9 times) plus itself 9 times)
+*/
+int powten(int n) {
+  assert(n <= NUMERIC_PLACES);
+
+  if (n == 0) {
+    return 1;
+  } else if (n == 1) {
+    return 10;
+  }
+
+  int base = 10;
+  int i, j;
+  int sum = 10;
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < 9; j++) {
+      sum += base;
+    }
+    base = sum;
+  }
+  return sum;
 }
 
 int render_board(char fill[], int len) {
@@ -324,89 +381,130 @@ int render_board(char fill[], int len) {
   return 0;
 }
 
-/* Fill the board with the terminal position of current row.
-   Return offset to caller.
+/* Set directions[direction] to -1 if it's invalid based on other parameters:
+
+   direction is the **one** index we are interested in keeping or resetting.
+   directions are the eight indices around the position, clockwise. 0 is northwest.
+   {min,max}_extents_index represent the valid index range for current row.
+   max_i--which may not be completely known yet--is present frontier of row index.
+   max_i_calculated is a flag to tell us if max_rows can be depended on.
+   position is the index on the board for the candidate letter.
+
+   ! max_rows may not be completely calculated yet
+
+   Return 0 on success, 1 if reset.
 */
-int fill_positions(int i, int start, char fill[]) {
-  int numeral = -1;
-  char digit = '?';
-  int exp;
-  int pos = start;
-  int tens_counter = NUMERIC_PLACES - 1; /* ugh, minus 1 on a constant */
+int reset_direction_bound(int directions[], int position, int direction,
+  int min_extents_index, int max_extents_index, int max_i, int max_i_calculated) {
 
-  /*
-     173 is 100 + 70 + 3
-     173 has 3 NUMERIC_PLACES
-     NUMERIC_PLACES is 0-based: 2, 1, 0
-     173 / 100 = 1   173 / (10^2) = 1
-     173 % 100 = 73  173 % (10^2) = 73
-     73 / 10 = 7     73 / (10^1) = 7
-     73 % 10 = 3     73 % (10^1) = 3
-     3 / 1 = 3       3 / (10^0) = 3
-     3 % 1 = 0       3 % (10^0) = 0
-   */
+  assert(direction >= 0 && direction <= 7);
+  assert(directions[direction] >= 0);
+  assert(max_i_calculated == 0 || max_i_calculated == 1);
+  /* assert(position >= min_extents_index && position <= max_extents_index); */
 
-  while (tens_counter >= 0) {
-    exp = powten(tens_counter);
-    if (exp > i) {
-      numeral = i;
-    } else {
-      numeral = i / exp;
-    }
+  int change_count = 0;
+  int orig_index = directions[direction];
 
-    digit = ntoc(numeral);
-
-    if (exp > i && tens_counter > 0) {
-      fill[pos++] = '_';
-    } else {
-      fill[pos++] = digit;
-    }
-    i = i % exp;
-    tens_counter--;
+  switch (direction) {
+    case 0: /* northwest */
+      break;
+    case 1: /* north */
+      break;
+    case 2: /* northeast */
+      break;
+    case 3: /* east */
+      printf("Check east.\n");
+      if (directions[direction] > max_extents_index) {
+        directions[direction] = -1;
+        change_count++;
+      }
+      break;
+    case 4: /* se */
+      break;
+    case 5: /* s */
+      break;
+    case 6: /* sw */
+      break;
+    case 7: /* w */
+      if (position == min_extents_index && directions[direction] < min_extents_index) {
+        directions[direction] = -1;
+        change_count++;
+      }
+      break;
+    default: assert(1==2);
   }
-  return pos - start;
-}
 
-/* Convert numeral integer to its character representation. */
-char ntoc(int numeral) {
-  assert(numeral < 10);
-  switch (numeral) {
-    case 0: return '0';
-    case 1: return '1';
-    case 2: return '2';
-    case 3: return '3';
-    case 4: return '4';
-    case 5: return '5';
-    case 6: return '6';
-    case 7: return '7';
-    case 8: return '8';
-    case 9: return '9';
-    default: assert(1 == 2);
-  }
-}
+  assert(change_count <= 1);
+  assert(directions[direction] == orig_index || directions[direction] == -1);
 
-/* 10^0 = 1 (base case)
-   10^1 = 10 * 1 = 10 = 10 plus itself 0 times
-   10^2 = 10 * 10 = 100 = 10 plus itself 9 times
-   10^3 = 10 * 10 * 10 = 1000 = ((10 plus itself 9 times) plus itself 9 times)
-*/
-int powten(int n) {
-  assert(n <= NUMERIC_PLACES);
-
-  if (n == 0) {
+  if (directions[direction] < 0) {
+    printf("Reset direction %d at %d.\n", direction, orig_index);
     return 1;
-  } else if (n == 1) {
-    return 10;
   }
 
-  int base = 10;
-  int i, j;
-  int sum = 10;
-  for (i = 0; i < n; i++) {
-    for (j = 0; j < 9; j++) {
-      sum += base;
+  return 0;
+}
+
+/* A "unit ruler" based on the first row */
+/* Treated as an offset, it becomes a window like so:
+
+               0             1             2             3             4
+   A A A A A A _ x x x x x _ _ x x x x _ _ _ x x x _ _ _ _ x x _ _ _ _ _ x
+   A A A A A A             x           x x         x x x       x x x x
+   A A A A A A
+   A A A A A A           5
+               _ _ _ _ _ _
+               x x x x x
+
+   Underscores are before the given index, inclusive of index.
+   The x's always cover or highlight one before the index, in the next row.
+
+   (The last row is a special case.)
+
+   Now we can build up some semantics. Based on surroundings,
+
+     0 1 2 Direction
+     7   3   0 (nw): ?
+     6 5 4   1 (up): ?
+             2 (ne): ?
+             3 (right): index+1
+             4 (se): offset+2
+             5 (down): offset+1
+             6 (sw): offset
+             7 (left): index-1
+
+   What does the window look like as a negative offset?
+
+     x x x x x     x x x x       x x x         x x           x
+   0             1             2             3             4             5
+   _           x _         x x _       x x x _     x x x x _   x x x x x _
+
+   We can fill in the rest, with a caveat (*) for last position in a row:
+
+     0 (nw): -offset-2
+     1 (up): -offset-1
+     2 (ne): -offset (*)
+
+   At the last position in the row, ne is invalid (and technically se too).
+
+   The last index in a row should always be equal to
+
+     row-size = offset + 1
+     try-last-row-index = ([0, 1, ..., LEN-1] * row-size) + offset
+
+   Where we would loop over 0..LEN-1.
+
+   Concretely, our sample board above has last-row indices of 5, 11, 17, and 23.
+*/
+int row_offset() {
+  assert(LEN > 1); /* Avoid pathological size-1 "boards." */
+  assert(adjacents[0] == '3'); /* All boards start with a "nw corner." */
+  int i;
+  for (i = 1; i < LEN; i++) {
+    if (adjacents[i] == '3') {
+      break;
     }
-    base = sum;
   }
-  return sum;
+  assert(i < LEN); /* All boards end with a "se corner." */
+  return i;
 }
